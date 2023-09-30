f79b9052af85d06a149cbad03483a676cff14f89
Marcos Barcia <marcos.b.iriarte@gmail.com>
2023-09-30 08:22:54 -0300

    first commit

--- EjemploEcommerce-main/src/services/products.service.js
+++
@@ -1,8 +0,0 @@
-import Product from '../models/Product.js';
-import GenericQueries from './genericQueries.js'
-
-export default class ProductService extends GenericQueries{
-    constructor(dao){
-        super(dao,Product.model);
-    }
-}
--- EjemploEcommerce-main/src/services/users.service.js
+++
@@ -1,8 +0,0 @@
-import User from "../models/User.js";
-import GenericQueries from './genericQueries.js'
-
-export default class UserService extends GenericQueries{
-    constructor(dao){
-        super(dao,User.model);
-    }
-}
